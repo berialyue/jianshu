9f2b80bed8017e8a0afc589f694d42ddd9f065d1
berialyue <berialyue@gmail.com>
2018-11-09 17:48:07 +0800

    add store

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
+import store from './store'
 import { Input, Button, List } from 'antd'
 import 'antd/dist/antd.css'
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-]
 
 class TodoList extends Component {
+  constructor(props){
+    super(props)
+    this.state = store.getState()
+  }
   render() {
     return (
       <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}/>
-        <Button type="primary">提交</Button>
+        <div>
+          <Input
+            value={this.state.inputValue}
+            placeholder='todo info' 
+            style={{width: '300px', marginRight: '10px'}}
+          />
+          <Button type="primary">提交</Button>
+          </div>
         <List
           style={{marginTop: '10px', width: '300px'}}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => (<List.Item>{item}</List.Item>)}
         />
       </div>
