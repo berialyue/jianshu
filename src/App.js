b8bf13e24391b4aed880b7f529d0020246594b33
berialyue <berialyue@gmail.com>
2018-11-22 13:42:10 +0800

    add redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component, Fragment } from 'react'
 import { GlobalStyle } from './style'
 import { IconFont } from './statics/iconfont/iconfont'
 import Header from './common/header'
+import store from './store'
+import { Provider } from 'react-redux'
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <GlobalStyle />
-        <IconFont />
-        <Header></Header>
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <GlobalStyle />
+          <IconFont />
+          <Header></Header>
+        </Fragment>
+      </Provider>
     )
   }
 }
