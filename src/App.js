29b5072c721d79275645269787f99e0d540cbfb8
berialyue <berialyue@gmail.com>
2018-12-07 23:11:38 +0800

    add components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { GlobalStyle } from './style'
 import { IconFont } from './statics/iconfont/iconfont'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './common/header'
+import Home from './pages/home'
+import Detail from './pages/detail'
 import store from './store'
 import { Provider } from 'react-redux'
 
@@ -16,8 +18,8 @@ class App extends Component {
           <Header></Header>
           <BrowserRouter>
             <div>
-              <Route path='/' exact render={() => <div>home</div>}></Route>
-              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+              <Route path='/' exact component={Home}></Route>
+              <Route path='/detail' exact component={Detail}></Route>
             </div>
             </BrowserRouter>
         </Fragment>
---
+++ src/pages/home/index.js
@@ -0,0 +1,30 @@
+import React,{ Component } from 'react'
+import Topic from './components/Topic'
+import List from './components/List'
+import Recommend from './components/Recommend'
+import Writer from './components/Writer'
+import { 
+  HomeWrapper, 
+  HomeLeft, 
+  HomeRight 
+} from './style'
+
+class Home extends Component {
+  render() {
+    return (
+      <HomeWrapper>
+        <HomeLeft>
+          <img className='banner-img' src="//upload.jianshu.io/admin_banners/web_images/4581/14d959ff757f21616baa933fd3bdbfcf52fd512a.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt=""/>
+          <Topic></Topic>
+          <List></List>
+        </HomeLeft>
+        <HomeRight>
+            <Recommend></Recommend>
+            <Writer></Writer>
+        </HomeRight>
+      </HomeWrapper>
+    )
+  }
+}
+
+export default Home
