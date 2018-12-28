fb5f5a2b206e8e7e378596b2e848b7856f9ecf55
berialyue <berialyue@gmail.com>
2018-12-28 21:57:58 +0800

    add detail.json

--- src/pages/detail/store/actionCreators.js
+++ src/pages/detail/store/actionCreators.js
@@ -2,3 +2,17 @@ import axios from 'axios'
 import * as constants from './constants'
 import { fromJS } from 'immutable'
 
+const changeDetail = (title, content) => ({
+  type: constants.CHANGE_DETAIL,
+  title,
+  content
+})
+
+export const getDetail = (page) => {
+  return (dispatch) => {
+    axios.get(`/api/detail.json`).then((res) => {
+      const result = res.data.data
+      dispatch(changeDetail(result.title, result.content))
+    })
+  }
+}
--- src/pages/detail/store/constants.js
+++ src/pages/detail/store/constants.js
@@ -1,3 +1,1 @@
-export const CHANGE_HOME_DATA = 'home_CHANGE_HOME_DATA'
-export const ADD_ARTICLE_LIST = 'home_ADD_ARTICLE_LIST'
-export const TOGGLE_TOP_SHOW = 'home_TOGGLE_TOP_SHOW'
+export const CHANGE_DETAIL = 'detail_CHANGE_DETAIL'
